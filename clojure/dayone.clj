6a412a79f981f912572c1808deb433b993b1f0ba
Riuchando <sdkinser456@gmail.com>
2015-12-03 00:19:53 -0600

    part two

--- clojure/dayone.clj
+++ clojure/dayone.clj
@@ -1,7 +1,24 @@
 ;should remove my class path/ learn how to read from working directory
 (defn dayone [filename] (let [ file (frequencies(slurp filename ))]
               (- (second (first  file)) (second( second file)))))
-(dayone "/Users/stephenkinser/Projects/ss-advent-solutions/clojure/dayone.txt")
-; (second (first (frequencies (slurp "/Users/stephenkinser/Projects/ss-advent-solutions/clojure/dayone.txt"))))
-;  (second (second (frequencies (slurp "/Users/stephenkinser/Projects/ss-advent-solutions/clojure/dayone.txt"))))
+(dayone (read-line))
 
+; (second (first (frequencies (slurp (read-line)))))
+
+(seq (slurp read-line))
+(defn curly [string] (if (= \( string) 1 -1))
+;(map curly (seq (slurp read-line)))
+;(= \( (first(seq (slurp read-line))))
+;the elegant solution is in this line of code
+;(take-while #(> % 0) (iterate inc (map curly(seq (slurp read-line)))))
+;(iterate (partial + 2) 0)
+
+(defn recursion[item pos sum]
+  (if ( = (+(first item) sum) -1) pos
+    (recursion (rest item) (+ pos 1) (+ sum (first item)))))
+
+
+(recursion   (map curly(seq (slurp read-line))) 1 0)
+
+;(reduce +(range 1 100))
+(;take-while #(> (gz %) 0)  (map curly(seq (slurp read-line))))
