5fc165667562ab81cd5045d0b244e17572f6e6f9
Steven Sheffey <stevensheffey4@gmail.com>
2015-12-04 10:53:39 -0600

    Added solutions to day 4

---
+++ Python/day4/advent4.2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+"""
+Solves the second problem in the fourth pair of advent of code problems
+Copyright 2015 Steven Sheffey
+
+    This file is part of ss-advent-solutions
+
+    ss-advent-solutions is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ss-advent-solutions is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ss-advent-solutions.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import md5
+
+data = "Insert key here"
+a=0
+while True:
+    if md5.new(data+str(a)).hexdigest()[:6] == '000000':
+        print a
+    a+=1
